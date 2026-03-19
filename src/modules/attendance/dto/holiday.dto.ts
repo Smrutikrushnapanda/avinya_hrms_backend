061975d8d2a7c45ad7f4abfa9154e705a2cf5384
Smrutikrushnapanda <skpanda017@gmail.com>
2026-03-19 17:31:41 +0530

    update

--- src/modules/attendance/dto/holiday.dto.ts
+++ src/modules/attendance/dto/holiday.dto.ts
@@ -1,14 +1,50 @@
+import { Type } from 'class-transformer';
+import {
+  IsBoolean,
+  IsDateString,
+  IsOptional,
+  IsString,
+  IsUUID,
+  MaxLength,
+} from 'class-validator';
+
 export class CreateHolidayDto {
+  @IsUUID()
   organizationId: string;
+
+  @IsString()
+  @MaxLength(255)
   name: string;
+
+  @IsDateString()
   date: string | Date;
+
+  @IsOptional()
+  @IsString()
   description?: string;
+
+  @IsOptional()
+  @Type(() => Boolean)
+  @IsBoolean()
   isOptional?: boolean;
 }
 
 export class UpdateHolidayDto {
+  @IsOptional()
+  @IsString()
+  @MaxLength(255)
   name?: string;
+
+  @IsOptional()
+  @IsDateString()
   date?: string | Date;
+
+  @IsOptional()
+  @IsString()
   description?: string;
+
+  @IsOptional()
+  @Type(() => Boolean)
+  @IsBoolean()
   isOptional?: boolean;
 }
